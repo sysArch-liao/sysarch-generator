5eb8d3b91f630bc77b9634582eea3ba60f7a6d88
Albert <794561629@qq.com>
2019-07-08 14:46:21 +0800

    mybatis的org.apache.ibatis.io.Resources类的方法使用

--- src/main/java/com/sysarch/liao/GeneratorSqlmap.java
+++ src/main/java/com/sysarch/liao/GeneratorSqlmap.java
@@ -1,5 +1,6 @@
 package com.sysarch.liao;
 
+import org.apache.ibatis.io.Resources;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -16,11 +17,16 @@ public class GeneratorSqlmap {
 
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
-		ClassLoader classLoader = getClass().getClassLoader();
-		URL url = classLoader.getResource("generator-config/generatorConfig.xml");
-		File configFile = new File(url.getFile());
-		//指定 逆向工程配置文件
+		// 获取mybatis配置文件的File对象的三种方法 start
+		//方法一 通过java的api获取
+//		ClassLoader classLoader = getClass().getClassLoader();
+//		URL url = classLoader.getResource("generator-config/generatorConfig.xml");
+//		File configFile = new File(url.getFile());
+		// 方法二  通过绝对路径获取
 //		File configFile = new File("F:\\devmProject\\myProject\\sysarch-generator\\src\\main\\resources\\generatorConfig.xml");
+		// 方法三()  Resources是mybatis的一个类在org.apache.ibatis.io.Resources路径下面
+		File configFile = Resources.getResourceAsFile("generator-config/generatorConfig.xml");
+		// 获取mybatis配置文件的File对象的三种方法 end
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
